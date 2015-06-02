6b3812f09fab336a72dd37310fa8d3cd23d6e231
ailiefraser <cafraser@cs.ucsd.edu>
2015-06-02 10:33:29 -0700

    Updates to interface

--- javascripts/Voice.js
+++ javascripts/Voice.js
@@ -8,22 +8,21 @@ app.controller('Voice', function($scope, $resource, $timeout) {
      */
     function init () {
 		$scope.messages = [];
-		$scope.displayMessage = "";
+		$scope.loadMessages();
 		$scope.currentPage = 0;
-		
+		$scope.alertMessage = "I need help";
+		$scope.voiceStyle = 'UK English Male';
     }
 
-
-	$scope.currentScreen = 1;
-	$scope.toScreen2 = function () {
-		$scope.currentScreen = 2;
-	};
-	$scope.toScreen1 = function () {
-		$scope.currentScreen = 1;
-	};
-	
-	$scope.chooseMessage = function () {
+	$scope.loadMessages = function() {
+		// load all messages from DB into $scope.messages array
+		$scope.messages[0] = "I am tired";
+		$scope.messages[1] = "I am thirsty";
+		$scope.messages[2] = "Let's go outside";
+		$scope.messages[3] = "I am bored";
+		$scope.messages[4] = "I am hungry";
 		
+		/**
 		// load messages from file
 		var file = 'metadata/message.txt';
 		console.log(file);
@@ -39,34 +38,64 @@ app.controller('Voice', function($scope, $resource, $timeout) {
 			}
 		};
 		reader.readAsText(file);
-		
-		// should be read from file instead
-		$scope.messages[0] = "I am tired";
-		$scope.messages[1] = "I am thirsty";
-		$scope.messages[2] = "Let's go outside";
-		$scope.messages[3] = "I am bored";
-		$scope.messages[4] = "I am hungry";
-		
+		**/
+	}
+	
+	$scope.arrayToString = function(array) {
+		for (i = 0; i < 3; i++) {
+			// highlight ones on current page
+			index = $scope.currentPage * 3 + i;
+			array[index] = "<span style='color:dark green;'>" + array[index] + "</span>";
+			console.log(array[index]);
+		}
+		return array.join("<br/>");
+	};
+
+	$scope.currentScreen = 1;
+	$scope.toScreen2 = function () {
+		$scope.currentScreen = 2;
+	};
+	$scope.toScreen1 = function () {
+		$scope.currentScreen = 1;
+	};
+	
+	$scope.writeMessage = function () {
+		// open eyehome keyboard
+		// play message when the keyboard is closed
+	}
+	
+	$scope.chooseMessage = function () {
+
 		$scope.toScreen2();
 	};
 	
 	$scope.nextPage = function () {
 		if ($scope.messages.length > ($scope.currentPage * 3) + 3) {
 			$scope.currentPage++;
+		} else {
+			// wrap around to first page
+			$scope.currentPage = 0;
 		}
 	};
 	
 	$scope.messageSelected = function (message_num) {
 	
-		$scope.displayMessage = $scope.messages[$scope.currentPage*3 + message_num];
-		$scope.currentPage = 0; // reset current page
-		$scope.toScreen1();
+		$scope.playMessage($scope.messages[$scope.currentPage*3 + message_num]);
 	
 	};
 	
-	$scope.playMessage = function () {
-		responsiveVoice.speak($scope.displayMessage, 'UK English Male');
+	$scope.playAlert = function () {
+		$scope.playMessage($scope.alertMessage);
+	};
+	
+	$scope.playMessage = function (msg) {
+		responsiveVoice.speak(msg, $scope.voiceStyle);
 		
+	};
+	
+	$scope.backToMain = function () {
+		$scope.currentPage = 0;
+		$scope.toScreen1();
 	};
 	
 
@@ -81,9 +110,9 @@ $scope.keyPress = function(e, currentScreen) {
        	if (key == '81') //left
         	$scope.writeMessage();
       	else if (key == '87') //up
-          	$scope.chooseMessage();
+          	$scope.playAlert();
       	else if (key == '69') // right
-          	$scope.playMessage();
+          	$scope.chooseMessage();
       	else if (key == '82') // down
           	$scope.quit();
       	else
@@ -101,13 +130,11 @@ $scope.keyPress = function(e, currentScreen) {
     	else if (key == '82') // down
       	  	$scope.nextPage();
       	else if (key == '89') // disengage
-      		$scope.toScreen1();
+      		$scope.backToMain();
     	else
         	console.log(e.keyCode);
     	}
-   
-		console.log("current screen is " + currentScreen);
-	}
+   	}
 
     init();
 });
